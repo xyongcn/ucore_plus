356d7e487fa2abaa176803b623ebacf0cfaf4d1d
mfhraven <mfhraven@127.com>
2016-05-29 01:46:13 +0800

    sigset tmp version

--- ucore/src/kern-ucore/fs/fuse/freebsd_compat/sys/syscallsubr.h
+++ ucore/src/kern-ucore/fs/fuse/freebsd_compat/sys/syscallsubr.h
@@ -2,11 +2,12 @@
 #define	_FREEBSD_COMPAT_SYSCALLSUBR_H_
 
 #include "types.h"
+#include <signal.h>
 
 struct thread;
 
-static int kern_sigprocmask(struct thread *td, int how, sigset_t *set, sigset_t *oset, int old) {
-  return 0;
+static int kern_sigprocmask(struct thread *td, int how, fuse_sigset_t *set, fuse_sigset_t *oset, int old) {
+  return do_sigprocmask(how, set->__bits[0], oset->__bits[0]);
 }
 
 #endif
