1ead7e975678ddc029b81e3652aad959a5f54a3e
Tan Si Nan <i@tinytangent.com>
2017-10-19 21:48:26 +0800

    Add some NUMA stubs for MIPS.

--- ucore/src/kern-ucore/arch/mips/include.mk
+++ ucore/src/kern-ucore/arch/mips/include.mk
@@ -1,4 +1,3 @@
-ARCH_INLUCDES:=debug driver init mm libs process sync syscall trap glue-ucore 
+ARCH_INLUCDES:=debug driver init mm libs process sync syscall trap glue-ucore numa
 ARCH_CFLAGS := -mips1 -DMACH_QEMU -g  -EL -G0 -fno-delayed-branch -Wall -O0
 ARCH_LDFLAGS := -n -G 0 -static -EL
-
